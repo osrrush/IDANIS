4cf30138b768bc584110df4acf327fb153c94c17
osrrush <34876091+osrrush@users.noreply.github.com>
2021-10-10 14:22:49 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+      <h1>Hola</h1>
+        <?php
+         echo "HOLA MUNDO!";
+        ?>
+    </body>
+</html>
